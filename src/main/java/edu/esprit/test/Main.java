cb45601fe3e95aa758685c2191160bd18388c6cd
Rayan <mohamedrayan.fathallah@esprit.tn>
2024-02-12 14:45:32 +0100

    Fifth commit corrected main

--- src/main/java/edu/esprit/test/Main.java
+++ src/main/java/edu/esprit/test/Main.java
@@ -15,19 +15,17 @@ public class Main {
         tournois t = new tournois("La Ligua","Tunis Rue Habib Borguiba", 16, "20/05/2023", "05/06/2023");
         ServiceTournois servicee = new ServiceTournois();
         servicee.ajouter(t);
-        servicee.supprimer(7);
-        servicee.afficher();
+        servicee.supprimer(13);
         tournois t1 = new tournois("Champions","Ariana", 20, "10/05/2024", "05/06/2023");
-        servicee.modifier(6,t1);
+        servicee.modifier(12,t1);
         servicee.afficher();
 
         matchs m = new matchs("25/05/2023","90 min");
         ServiceMatch service = new ServiceMatch();
         service.ajouter(m);
-        service.supprimer(4);
-        service.afficher();
+        service.supprimer(12);
         matchs m1 = new matchs("11/05/2023","90 min");
-        service.modifier(9,m1);
+        service.modifier(13,m1);
         service.afficher();
 
     }
